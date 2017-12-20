792d664b0f7a53f719cf7173f6505eb98f9c8198
sc-AlexanderNg <alex@sitecore.net>
2017-12-20 13:12:07 +0800

    Modify function _removeRendering

---
+++ src/Sitecore.Support.54721/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.54721")]
+[assembly: AssemblyProduct("Sitecore.Support.54721")]
+[assembly: ComVisible(false)]
